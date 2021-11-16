fc6c1328afa6cc69b7cafe6ee53677221e1be822
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-11-15 20:12:03 -0500

    building basic structure of shop page

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./app.css";
 
 export default function App() {
+	const [products, setProducts] = useState({});
 	return (
 		<div className="App">
-			<h1>LaCava Studios</h1>
+			{/* Nav Bar */}
+			<nav>
+				<h1>LaCava Studios</h1>
+			</nav>
+			<div className="shop-container">
+				{/* Load all products from db and display */}
+			</div>
 		</div>
 	);
 }
