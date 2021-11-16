72cf468d7a6fcf03840bbd3f7861d1d66e0330bc
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-11-15 20:40:08 -0500

    refactored shop container into its own container, hard coded products

--- src/components/shop-container/shop-container.tsx
+++ src/components/shop-container/shop-container.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import "./shop-container.css";
 
 export const ShopContainer = () => {
-	return <div className="shop-container">Shop</div>;
+	// this needs to be async
+	// const fetchProductsForSale = () => {
+	// 	// fetch products
+	// };
+
+	return (
+		<div className="shop-section">
+			<h1 className="shop-title">Shop</h1>
+			<div className="shop-container">
+				<div className="product-1"></div>
+				<div className="product-2"></div>
+				<div className="product-3"></div>
+				<div className="product-4"></div>
+				<div className="product-5"></div>
+			</div>
+		</div>
+	);
 };
