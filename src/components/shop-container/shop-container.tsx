63be60724d1b14e1e8f9d1c159ad3c69fc66d6b1
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-11-16 20:57:32 -0500

    added styling to hard coded products for mobile dimentions

--- src/components/shop-container/shop-container.tsx
+++ src/components/shop-container/shop-container.tsx
@@ -11,11 +11,46 @@ export const ShopContainer = () => {
 		<div className="shop-section">
 			<h1 className="shop-title">Shop</h1>
 			<div className="shop-container">
-				<div className="product-1"></div>
-				<div className="product-2"></div>
-				<div className="product-3"></div>
-				<div className="product-4"></div>
-				<div className="product-5"></div>
+				<div className="product">
+					<img
+						src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
+						alt="Glass Straws"
+					/>
+					<p>Product 1 Name</p>
+					<p>$500</p>
+				</div>
+				<div className="product">
+					<img
+						src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
+						alt="Glass Straws"
+					/>
+					<p>Product 2 Name</p>
+					<p>$600</p>
+				</div>
+				<div className="product">
+					<img
+						src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
+						alt="Glass Straws"
+					/>
+					<p>Product 3 Name</p>
+					<p>$700</p>
+				</div>
+				<div className="product">
+					<img
+						src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
+						alt="Glass Straws"
+					/>
+					<p>Product 4 Name</p>
+					<p>$800</p>
+				</div>
+				<div className="product">
+					<img
+						src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
+						alt="Glass Straws"
+					/>
+					<p>Product 5 Name</p>
+					<p>$900</p>
+				</div>
 			</div>
 		</div>
 	);
