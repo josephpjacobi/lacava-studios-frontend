2423cfa11e3eb07cbf22ad24480910a37d721c61
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-11-18 19:49:27 -0500

    refactored product code into its own folder and imported to shop-container

---
+++ src/components/product/product.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./product.css";
+
+export const Product = () => {
+	return (
+		<div className="product">
+			<img
+				src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
+				alt="Glass Straws"
+			/>
+			<p>Product 1 Name</p>
+			<p>$500</p>
+		</div>
+	);
+};
--- src/components/shop-container/shop-container.tsx
+++ src/components/shop-container/shop-container.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Product } from "../product/product";
 import "./shop-container.css";
 
 export const ShopContainer = () => {
@@ -6,46 +7,11 @@ export const ShopContainer = () => {
 		<div className="shop-section">
 			<h1 className="shop-title">Shop</h1>
 			<div className="shop-container">
-				<div className="product">
-					<img
-						src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
-						alt="Glass Straws"
-					/>
-					<p>Product 1 Name</p>
-					<p>$500</p>
-				</div>
-				<div className="product">
-					<img
-						src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
-						alt="Glass Straws"
-					/>
-					<p>Product 2 Name</p>
-					<p>$600</p>
-				</div>
-				<div className="product">
-					<img
-						src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
-						alt="Glass Straws"
-					/>
-					<p>Product 3 Name</p>
-					<p>$700</p>
-				</div>
-				<div className="product">
-					<img
-						src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
-						alt="Glass Straws"
-					/>
-					<p>Product 4 Name</p>
-					<p>$800</p>
-				</div>
-				<div className="product">
-					<img
-						src="https://cdn.shopify.com/s/files/1/0607/6570/0305/products/IMG_0165_470x.jpg?v=1635547708"
-						alt="Glass Straws"
-					/>
-					<p>Product 5 Name</p>
-					<p>$900</p>
-				</div>
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
 			</div>
 		</div>
 	);
