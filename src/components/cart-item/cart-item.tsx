6cc15392602c38c0081b3394522f6669a5c82f51
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-11-22 20:55:12 -0500

    added hardcoded cart item data

--- src/components/cart-item/cart-item.tsx
+++ src/components/cart-item/cart-item.tsx
@@ -3,6 +3,13 @@ import { CartItem } from "../../app";
 import "./cart-item.css";
 
 // CartItemDisplay component name needs to be changed
-export const CartItemDisplay = ({ description }: CartItem) => (
-	<div className="cart-item-container">{description}</div>
-);
+export const CartItemDisplay = ({ description }: CartItem) => {
+	return (
+		<div className="cart-item-container">
+			<h3>Name: Product Name</h3>
+			<h5>Quantity: Quantity</h5>
+			<p>{description}</p>
+			<button>Remove</button>
+		</div>
+	);
+};
